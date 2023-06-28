4d1b71a9da71206accd2c3bad3c398a8b3867791
Iryna Lypnyk <iryna.lypnyk@gmail.com>
2023-06-29 00:19:41 +0100

    level 3 - within each individual table, show the highest scores first

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,11 +1,12 @@
-import React from "react";
+import React, { useMemo } from "react";
 
 function Card({ country }) {
+    const scoresSorted = useMemo(() => country.scores.sort((a, b) => b.s - a.s), [country.scores]);
     return (
         <div className="card-container">
             <h3 className="card-title">High scores: {country.name}</h3>
             <ul className="card-list">
-                {country.scores.map((team) => {
+                {scoresSorted.map((team) => {
                     return (
                         <li className="card-item" key={team.n}>
                             <span className="card-half-item card-team-name">{team.n.toUpperCase()}</span>
